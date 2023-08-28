3900618797de6cdceeafb7a6b3507cd4a9d02786
A VIDHYA SAGAR <sagarvidhya082002@gmail.com>
2023-08-28 20:09:37 +0530

    intial commit

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+import { createUser, deleteUser, getUser, getUsers, updateUser } from '../controllers/users.js';
+
+
+const router = express.Router();
+
+
+
+
+
+router.post('/',createUser);
+router.get('/',getUsers);
+
+router.get('/:id',getUser);
+
+router.delete('/:id',deleteUser)
+
+router.patch('/:id',updateUser)
+
+export default router;
